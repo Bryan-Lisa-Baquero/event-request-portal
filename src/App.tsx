d4d93d6ee6453a11e522d2b3668dd688aab323e1
Bryan-Lisa-Baquero <bryanbaquero67@gmail.com>
2026-01-29 16:03:38 -0500

    Bypass MSAL when VITE_DISABLE_AUTH=true

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import './App.css'
-import Nav from './components/Navbar'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { MsalAuthenticationTemplate } from '@azure/msal-react'
-import { InteractionType } from '@azure/msal-browser'
-import Constants from './constants'
-import AppRoutes from './AppRoutes'
+import { DISABLE_AUTH } from "./config/auth";
+import "./App.css";
+import Nav from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import { MsalAuthenticationTemplate } from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser";
+import Constants from "./constants";
+import AppRoutes from "./AppRoutes";
 
 function App() {
+  if (DISABLE_AUTH) {
+    return (
+      <Router>
+        <Nav />
+        <AppRoutes />
+      </Router>
+    );
+  }
 
   return (
     <MsalAuthenticationTemplate
-          interactionType={InteractionType.Redirect}
-          authenticationRequest={{ scopes: [ Constants.apiScope ] }}
-      >
+      interactionType={InteractionType.Redirect}
+      authenticationRequest={{ scopes: [Constants.apiScope] }}
+    >
       <Router>
         <Nav />
-        <AppRoutes />      
+        <AppRoutes />
       </Router>
     </MsalAuthenticationTemplate>
-  )
+  );
 }
 
-export default App
+export default App;
