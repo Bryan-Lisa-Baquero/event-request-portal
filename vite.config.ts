46bf5f7ac0281bc1091baf70934dc2364c70a528
Bryan-Lisa-Baquero <bryanbaquero67@gmail.com>
2026-01-29 14:25:11 -0500

    Fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,25 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-build: {
-  emptyOutDir: true,
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        react: ['react', 'react-dom'],
-        msal: ['@azure/msal-browser', '@azure/msal-react'],
-        aggrid: ['ag-grid-react']
-      }
-    }
-  }
-},
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          msal: ["@azure/msal-browser", "@azure/msal-react"],
+          aggrid: ["ag-grid-react"],
+        },
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:5246", // dev only
+    },
+  },
+});
